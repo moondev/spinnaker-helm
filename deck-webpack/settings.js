d0fb5e847a92011953152c204c11ae8ea2ba3b91
Chad Moon <cmoon@kenzan.com>
2017-01-10 22:45:13 -0700

    init

---
+++ deck-webpack/settings.js
@@ -0,0 +1,126 @@
+webpackJsonp([1,3],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(process) {'use strict';
+
+	var feedbackUrl = process.env.FEEDBACK_URL;
+	var gateHost = "/gate" || 'http://localhost:8084';
+	var bakeryHost = process.env.BAKERY_HOST || 'http://localhost:8087';
+	var bakeryDetailUrl = "/bakery" || bakeryHost + '/#/?region={{context.region}}&package={{context.package}}&detail=bake:{{context.status.resourceId}}';
+	var authEndpoint = process.env.AUTH_ENDPOINT || gateHost + '/auth/user';
+	var authEnabled =  true ? false : true;
+	var netflixMode = process.env.NETFLIX_MODE === 'true' ? true : false;
+	var chaosEnabled = netflixMode || process.env.CHAOS_ENABLED === 'true' ? true : false;
+	var fiatEnabled = process.env.FIAT_ENABLED === 'true' ? true : false;
+	var entityTagsEnabled = process.env.ENTITY_TAGS_ENABLED == 'true' ? true : false;
+
+	window.spinnakerSettings = {
+	  checkForUpdates: true,
+	  defaultProviders: ['aws', 'gce', 'azure', 'cf', 'kubernetes', 'titus', 'openstack'],
+	  feedbackUrl: feedbackUrl,
+	  gateUrl: gateHost,
+	  bakeryDetailUrl: bakeryDetailUrl,
+	  authEndpoint: authEndpoint,
+	  pollSchedule: 30000,
+	  defaultTimeZone: process.env.TIMEZONE || 'America/Los_Angeles', // see http://momentjs.com/timezone/docs/#/data-utilities/
+	  defaultCategory: 'serverGroup',
+	  defaultInstancePort: 80,
+	  providers: {
+	    azure: {
+	      defaults: {
+	        account: 'azure-test',
+	        region: 'westus'
+	      }
+	    },
+	    aws: {
+	      defaults: {
+	        account: 'test',
+	        region: 'us-east-1',
+	        iamRole: 'BaseIAMRole'
+	      },
+	      defaultSecurityGroups: [],
+	      loadBalancers: {
+	        // if true, VPC load balancers will be created as internal load balancers if the selected subnet has a purpose
+	        // tag that starts with "internal"
+	        inferInternalFlagFromSubnet: false
+	      },
+	      useAmiBlockDeviceMappings: false
+	    },
+	    gce: {
+	      defaults: {
+	        account: 'my-google-account',
+	        region: 'us-central1',
+	        zone: 'us-central1-f'
+	      }
+	    },
+	    titus: {
+	      defaults: {
+	        account: 'titustestvpc',
+	        region: 'us-east-1',
+	        iamProfile: '{{application}}InstanceProfile'
+	      }
+	    },
+	    openstack: {
+	      defaults: {
+	        account: 'test',
+	        region: 'us-west-1'
+	      }
+	    },
+	    kubernetes: {
+	      defaults: {
+	        account: 'my-kubernetes-account',
+	        namespace: 'default'
+	      }
+	    },
+	    appengine: {
+	      defaults: {
+	        account: 'my-appengine-account'
+	      }
+	    }
+	  },
+	  whatsNew: {
+	    gistId: '32526cd608db3d811b38',
+	    fileName: 'news.md'
+	  },
+	  notifications: {
+	    email: {
+	      enabled: true
+	    },
+	    hipchat: {
+	      enabled: true,
+	      botName: 'Skynet T-800'
+	    },
+	    sms: {
+	      enabled: true
+	    },
+	    slack: {
+	      enabled: true,
+	      botName: 'spinnakerbot'
+	    }
+	  },
+	  authEnabled: authEnabled,
+	  authTtl: 600000,
+	  gitSources: ['stash', 'github'],
+	  triggerTypes: ['git', 'pipeline', 'docker', 'cron', 'jenkins'],
+	  feature: {
+	    entityTags: entityTagsEnabled,
+	    fiatEnabled: fiatEnabled,
+	    pipelines: true,
+	    notifications: false,
+	    fastProperty: true,
+	    vpcMigrator: true,
+	    clusterDiff: false,
+	    roscoMode: false,
+	    netflixMode: netflixMode,
+	    chaosMonkey: chaosEnabled,
+	    // whether stages affecting infrastructure (like "Create Load Balancer") should be enabled or not
+	    infrastructureStages: process.env.INFRA_STAGES === 'enabled',
+	    jobs: false,
+	    snapshots: false
+	  }
+	};
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(570)))
+
+/***/ }
+]);
---
+++ static/config/packer/install_packages.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Make the build fail on errors.
+set -e
+
+# Strip the first part to avoid credentials leaks.
+echo "repository=$(echo $repository | sed s/^.*@//g)"
+echo "package_type=$package_type"
+echo "packages=$packages"
+echo "upgrade=$upgrade"
+
+# Strip leading/trailing quotes if present.
+repository=`echo $repository | sed 's/^"\(.*\)"$/\1/'`
+
+# Strip leading/trailing quotes if present.
+# Also convert a comma-separated list to a whitespace-separated one.
+packages=`echo $packages | sed 's/^"\(.*\)"$/\1/' | sed 's/,/ /g'`
+
+
+function provision_deb() {
+  # https://www.packer.io/docs/builders/amazon-chroot.html look at gotchas at the end.
+  if [[ "$disable_services" == "true" ]]; then
+    echo "creating /usr/sbin/policy-rc.d to prevent services from being started"
+    echo '#!/bin/sh' | sudo tee /usr/sbin/policy-rc.d > /dev/null
+    echo 'exit 101' | sudo tee -a /usr/sbin/policy-rc.d > /dev/null
+    sudo chmod a+x /usr/sbin/policy-rc.d
+  fi
+
+  if [[ "$repository" != "" ]]; then
+    IFS=';' read -ra repo <<< "$repository"
+    for i in "${repo[@]}"; do
+      echo "deb $i" | sudo tee -a /etc/apt/sources.list.d/spinnaker.list > /dev/null
+    done
+  fi
+
+  sudo apt-get update
+  if [[ "$upgrade" == "true" ]]; then
+    sudo unattended-upgrade -v
+  fi
+
+  # Enforce the package installation order.
+  for package in $packages; do sudo DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y $package; done
+
+  # https://www.packer.io/docs/builders/amazon-chroot.html look at gotchas at the end.
+  if [[ "$disable_services" == "true" ]]; then
+    echo "removing /usr/sbin/policy-rc.d"
+    sudo rm -f /usr/sbin/policy-rc.d
+  fi
+
+  if [[ "$repository" != "" ]]; then
+    # Cleanup repository configuration
+    sudo rm /etc/apt/sources.list.d/spinnaker.list
+  fi
+}
+
+function provision_rpm() {
+  if [[ "$repository" != "" ]]; then
+    cat > /tmp/spinnaker.repo <<EOF
+[spinnaker]
+name=spinnaker
+baseurl=$repository
+gpgcheck=0
+enabled=1
+EOF
+    sudo mv /tmp/spinnaker.repo /etc/yum.repos.d/
+  fi
+
+  if [[ "$upgrade" == "true" ]]; then
+    sudo yum -y update
+  fi
+
+  # Enforce the package installation order.
+  for package in $packages; do sudo yum -y install $package; done
+
+  if [[ "$repository" != "" ]]; then
+    # Cleanup repository configuration
+    sudo rm /etc/yum.repos.d/spinnaker.repo
+  fi
+}
+
+function main() {
+  if [[ "$package_type" == "deb" ]]; then
+    provision_deb
+  elif [[ "$package_type" == "rpm" ]]; then
+    provision_rpm
+  fi
+}
+
+main
