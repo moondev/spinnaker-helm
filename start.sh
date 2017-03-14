188f838c2200896001712838896281b9eedeee4d
Chad Moon <chad.d.moon@gmail.com>
2017-03-14 13:57:32 -0600

    first run

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-#sudo virsh net-start default
+sudo virsh net-start default
 
 minikube delete
 
 rm -rf ~/.minikube
 
 minikube start --vm-driver kvm --memory 8000 --cpus 4 --disk-size 40g
+
+
+
+
+helm install ./chart
